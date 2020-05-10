bbb772d57d3c952ac8763b8c59a8380413f48f38
Harald Maida <harald.maida@innoave.com>
2020-05-10 16:39:39 +0200

    start writing the readme

--- README.md
+++ README.md
@@ -20,40 +20,19 @@
 
 **Event sourcing and CQRS for Rust applications**
 
+> very much work in progress!
+
 ```toml
 [dependencies]
 eventmill = "0.1"    
 ```
 
+## TODO
 
-```text
-    (state, event) -> state
-```
-
-```text
-    (Aggregate, Command) -> Vec<NewEvent>
-```
-```text
-    (NewEvent, Store) -> DomainEvent
-```
-```text
-    (Aggregate(n), DomainEvent) -> Aggregate(n+1)
-```
-
-
-## Dispatch a Command
-
-Given a command we need to find the right aggregate instance to handle it.
-
-When dispatcher gets the aggregate instance it calls the handle_command function. The returned
-list of events (`Vec<NewEvent>`) will be enriched with the address properties of the aggregate to
-get the related `DomainEvent`.
-
-```rust
-    trait DispatchCommand<C, S> {
-        type Aggregate: HandleCommand<C, Self> + WithAggregateId;
-        type Error;
-
-        fn dispatch_command(command: C, context: S) -> Result<Vec<DomainEvent<E, AggregateIdOf<Self::Aggregate>>>, Self::Error>;
-    }
-```  
+* [X] define basic abstractions and API
+* [X] provide a first example on how it looks like to use the API
+* [ ] make more examples to polish the API
+* [ ] write rust-doc for the API
+* [ ] support async/await or switch to async as the only option
+* [ ] consider providing default implementations for eventstores and other building blocks
+* [ ] ...
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,47 @@
+//!
+//! ## Abstractions
+//!
+//! Event applied to an aggregate state results in a modified aggregate state:
+//!
+//! ```text
+//! (aggregate, Event) -> aggregate'
+//! ```
+//!
+//! Commands are handled by aggregates resulting in a zero, one or several events:
+//!
+//! ```text
+//! (Aggregate, Command) -> Vec<NewEvent>
+//! ```
+//!
+//! ```text
+//! (NewEvent, Store) -> DomainEvent
+//! ```
+//!
+//! Aggregates have a generation property. The generation of an aggregate enumerates the number of
+//! modifications through a sequence of applied events.
+//!
+//! Applying an event to an aggregate advances its state from the current generation n to the next
+//! generation n+1:
+//!
+//! ```text
+//! (Aggregate(n), DomainEvent) -> Aggregate(n+1)
+//! ```
+//!
+//! Aggregates can be replayed by applying all stored events to a newly initialized aggregate:
+//!
+//! ```text
+//! (initialized aggregate, &[DomainEvent]) -> current aggregate
+//! ```
+//!
+//!
+//! ## Dispatching a Command
+//!
+//! Given a command we need to find the right aggregate instance to handle it.
+//!
+//! When dispatcher gets the aggregate instance it calls the handle_command function. The returned
+//! list of events (`Vec<NewEvent>`) will be enriched with the address properties of the aggregate to
+//! get the related `DomainEvent`s.
+//!
 #![doc(html_root_url = "https://docs.rs/eventmill/0.1.0")]
 //#![deny(missing_docs)]
 #![deny(unsafe_code, unstable_features)]
