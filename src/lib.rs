64e18dc89cbd22671c262cd759c75155704696e4
Harald Maida <harald.maida@innoave.com>
2020-05-10 04:35:12 +0200

    add version-sync test

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
+#![doc(html_root_url = "https://docs.rs/eventmill/0.1.0")]
+//#![deny(missing_docs)]
+#![deny(unsafe_code, unstable_features)]
 #![warn(
     bare_trait_objects,
     missing_copy_implementations,
     missing_debug_implementations,
-//    missing_docs,
     rust_2018_idioms,
     trivial_casts,
     trivial_numeric_casts,
-    unsafe_code,
-    unstable_features,
     unused_extern_crates,
     unused_import_braces,
     unused_qualifications
