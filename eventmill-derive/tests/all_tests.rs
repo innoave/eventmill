82b04511a61f7f7cb680fd7cf354b1eb6e3f978a
Harald Maida <harald.maida@innoave.com>
2020-05-12 18:51:00 +0200

    add enum const test for derive macro

--- eventmill-derive/tests/all_tests.rs
+++ eventmill-derive/tests/all_tests.rs
@@ -9,4 +9,5 @@ fn build_struct_all_attributes_specified() {
     t.pass("tests/build_enum_event_type_attribute_on_variants.rs");
     t.compile_fail("tests/fail_struct_unknown_identifier_as_event_source.rs");
     t.pass("tests/build_struct_const_as_event_source.rs");
+    t.pass("tests/build_enum_const_as_event_type_version.rs");
 }
