667a11b694c7ede3b2258451b2ff5cd97256a72c
Harald Maida <harald.maida@innoave.com>
2020-05-16 09:40:34 +0200

    move sources of eventmill crate to subdirectory ./eventmill

--- eventmill/tests/version_numbers.rs
+++ eventmill/tests/version_numbers.rs
@@ -2,7 +2,7 @@ use version_sync::{assert_html_root_url_updated, assert_markdown_deps_updated};
 
 #[test]
 fn test_readme_deps() {
-    assert_markdown_deps_updated!("README.md");
+    assert_markdown_deps_updated!("../README.md");
 }
 
 #[test]
